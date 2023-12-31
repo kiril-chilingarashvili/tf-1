9247203f63b7e70b94ea4824055adcaebb4b6f20
Kiril Chilingarashvili <k.chilingarashvili@gmail.com>
2023-12-30 21:28:23 -0800

    latest

--- docs/python/W2/network.py
+++ docs/python/W2/network.py
@@ -33,3 +33,228 @@ test_images = test_images / 255.0
 model = tf.keras.models.Sequential([tf.keras.layers.Flatten(), 
                                     tf.keras.layers.Dense(128, activation=tf.nn.relu), 
                                     tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
+
+
+# Softmax example
+"""
+# Declare sample inputs and convert to a tensor
+inputs = np.array([[1.0, 3.0, 4.0, 2.0]])
+inputs = tf.convert_to_tensor(inputs)
+print(f'input to softmax function: {inputs.numpy()}')
+
+# Feed the inputs to a softmax activation function
+outputs = tf.keras.activations.softmax(inputs)
+print(f'output of softmax function: {outputs.numpy()}')
+
+# Get the sum of all values after the softmax
+sum = tf.reduce_sum(outputs)
+print(f'sum of outputs: {sum}')
+
+# Get the index with highest value
+prediction = np.argmax(outputs)
+print(f'class with highest probability: {prediction}')
+"""
+
+# compile the model
+model.compile(optimizer = tf.optimizers.Adam(),
+              loss = 'sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+# train the model
+model.fit(training_images, training_labels, epochs=5)
+
+# Evaluate the model on unseen data
+model.evaluate(test_images, test_labels)
+
+# exercises
+
+# 1
+classifications = model.predict(test_images)
+
+print(classifications[0], classifications[0].argmax(), classifications[0].max(), test_labels[0])
+
+# 2
+fmnist = tf.keras.datasets.fashion_mnist
+
+(training_images, training_labels) ,  (test_images, test_labels) = fmnist.load_data()
+
+training_images = training_images/255.0
+test_images = test_images/255.0
+
+model = tf.keras.models.Sequential([tf.keras.layers.Flatten(),
+                                    tf.keras.layers.Dense(512, activation=tf.nn.relu), # Try experimenting with this layer
+                                    tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
+
+model.compile(optimizer = 'adam',
+              loss = 'sparse_categorical_crossentropy')
+
+model.fit(training_images, training_labels, epochs=5)
+
+model.evaluate(test_images, test_labels)
+
+classifications = model.predict(test_images)
+
+print(classifications[0])
+print(test_labels[0])
+
+# 2.b. Try 1024 neurons
+fmnist = tf.keras.datasets.fashion_mnist
+
+(training_images, training_labels) ,  (test_images, test_labels) = fmnist.load_data()
+
+training_images = training_images/255.0
+test_images = test_images/255.0
+
+model = tf.keras.models.Sequential([tf.keras.layers.Flatten(),
+                                    tf.keras.layers.Dense(1024, activation=tf.nn.relu), # Try experimenting with this layer
+                                    tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
+
+model.compile(optimizer = 'adam',
+              loss = 'sparse_categorical_crossentropy')
+
+model.fit(training_images, training_labels, epochs=5)
+
+model.evaluate(test_images, test_labels)
+
+classifications = model.predict(test_images)
+
+print(classifications[0])
+print(test_labels[0])
+
+# 3 Remove flatten layer
+fmnist = tf.keras.datasets.fashion_mnist
+
+(training_images, training_labels) ,  (test_images, test_labels) = fmnist.load_data()
+
+training_images = training_images/255.0
+test_images = test_images/255.0
+
+model = tf.keras.models.Sequential([tf.keras.layers.Flatten(), # Try removing this layer (error)
+                                    tf.keras.layers.Dense(64, activation=tf.nn.relu),
+                                    tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
+
+model.compile(optimizer = 'adam',
+              loss = 'sparse_categorical_crossentropy')
+
+model.fit(training_images, training_labels, epochs=5)
+
+model.evaluate(test_images, test_labels)
+
+classifications = model.predict(test_images)
+
+print(classifications[0])
+print(test_labels[0])
+
+# 4 try with 5 classes
+fmnist = tf.keras.datasets.fashion_mnist
+
+(training_images, training_labels) ,  (test_images, test_labels) = fmnist.load_data()
+
+training_images = training_images/255.0
+test_images = test_images/255.0
+
+model = tf.keras.models.Sequential([tf.keras.layers.Flatten(),
+                                    tf.keras.layers.Dense(64, activation=tf.nn.relu),
+                                    tf.keras.layers.Dense(10, activation=tf.nn.softmax) # Try experimenting with this layer (error)
+                                  ])
+
+model.compile(optimizer = 'adam',
+              loss = 'sparse_categorical_crossentropy')
+
+model.fit(training_images, training_labels, epochs=5)
+
+model.evaluate(test_images, test_labels)
+
+classifications = model.predict(test_images)
+
+print(classifications[0])
+print(test_labels[0])
+
+# 5 Add more layers
+fmnist = tf.keras.datasets.fashion_mnist
+
+(training_images, training_labels) ,  (test_images, test_labels) = fmnist.load_data()
+
+training_images = training_images/255.0
+test_images = test_images/255.0
+
+model = tf.keras.models.Sequential([tf.keras.layers.Flatten(),
+                                    # Add a layer here,
+                                    tf.keras.layers.Dense(256, activation=tf.nn.relu),
+                                    tf.keras.layers.Dense(256, activation=tf.nn.relu),
+                                    # Add a layer here
+                                    tf.keras.layers.Dense(256, activation=tf.nn.relu),
+                                    tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+                                  ])
+
+model.compile(optimizer = 'adam',
+              loss = 'sparse_categorical_crossentropy')
+
+model.fit(training_images, training_labels, epochs=5)
+
+model.evaluate(test_images, test_labels)
+
+classifications = model.predict(test_images)
+
+print(classifications[0])
+print(test_labels[0])
+
+# 6 Add more epochs
+fmnist = tf.keras.datasets.fashion_mnist
+
+(training_images, training_labels) ,  (test_images, test_labels) = fmnist.load_data()
+
+training_images = training_images/255.0
+test_images = test_images/255.0
+
+model = tf.keras.models.Sequential([tf.keras.layers.Flatten(),
+                                    tf.keras.layers.Dense(128, activation=tf.nn.relu),
+                                    tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
+
+model.compile(optimizer = 'adam',
+              loss = 'sparse_categorical_crossentropy')
+
+model.fit(training_images, training_labels, epochs=30) # Experiment with the number of epochs
+
+model.evaluate(test_images, test_labels)
+
+# 7 Try without data normalization
+fmnist = tf.keras.datasets.fashion_mnist
+
+(training_images, training_labels) ,  (test_images, test_labels) = fmnist.load_data()
+
+# training_images=training_images/255.0 # Experiment with removing this line
+# test_images=test_images/255.0 # Experiment with removing this line
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(512, activation=tf.nn.relu),
+  tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy')
+model.fit(training_images, training_labels, epochs=5)
+model.evaluate(test_images, test_labels)
+classifications = model.predict(test_images)
+print(classifications[0])
+print(test_labels[0])
+
+# 8 Use callbacks
+class myCallback(tf.keras.callbacks.Callback):
+  def on_epoch_end(self, epoch, logs={}):
+    if(logs.get('accuracy') >= 0.9): # Experiment with changing this value
+      print("\nReached 60% accuracy so cancelling training!")
+      self.model.stop_training = True
+
+callbacks = myCallback()
+
+fmnist = tf.keras.datasets.fashion_mnist
+(training_images, training_labels) ,  (test_images, test_labels) = fmnist.load_data()
+
+training_images=training_images/255.0
+test_images=test_images/255.0
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(512, activation=tf.nn.relu),
+  tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.fit(training_images, training_labels, epochs=5, callbacks=[callbacks])
